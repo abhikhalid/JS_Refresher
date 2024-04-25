f1f62417b98e3b6bbb3efbeb4f2e199c1cbc3bf7
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-04-25 18:52:50 +0600

    Handling the esc key pressed

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+const openModal = function(){
+    // console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i =0; i< btnsOpenModal.length;i++){
+    // console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click',openModal);
+
+    btnCloseModal.addEventListener('click',closeModal);
+
+    overlay.addEventListener('click',closeModal);
+
+    document.addEventListener('keydown',function(e){
+        console.log(e.key);
+
+        // if(e.key === 'Escape') console.log('Esc was pressed');
+
+        if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+            //if the modal is currenly visible
+            if(!modal.classList.contains('hidden')){
+                closeModal();
+            }
+        }
+    });
+}
+
