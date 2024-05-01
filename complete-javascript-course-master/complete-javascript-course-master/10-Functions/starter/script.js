c10c693b1f5eedc8579e5ac86aa8740b4b015f76
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-05-01 17:54:45 +0600

    sec - 10 : A closer at functions - DONE

--- complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,5 +1,72 @@
 'use strict';
 
+/*
+///////////////////////////////////////
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
+
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+
+///////////////////////////////////////
 
 // Functions Accepting Callback Functions
 /*
@@ -268,7 +335,7 @@ console.log(notPrivate); //23
 
 // When a variable can't be found in the function scope, js will look into the backpack and take the missing variable from there.
 
-// We do not have to manually create closures. this is a javascript feature that happens automatically.  We can't even acess closed-over variables explicitly. A close is not a tangible js object. 
+// We do not have to manually create closures. this is a javascript feature that happens automatically.  We can't even acess closed-over variables explicitly. A close is not a tangible js object.
 
 /*
 const secureBooking = function () {
@@ -351,7 +418,7 @@ boardPassengers(100, 3);
 //     header.style.color = 'red';
 // })();
 
-// Solution : 
+// Solution :
 
 (function () {
     const header = document.querySelector(`h1`);
