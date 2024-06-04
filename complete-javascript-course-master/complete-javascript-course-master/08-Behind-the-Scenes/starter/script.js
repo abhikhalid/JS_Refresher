6544d3d88fbc0a90d5a8e63eda16db51b9b5a1ce
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-06-04 18:38:29 +0600

    Scoping in Practice.

--- complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -179,12 +179,16 @@ function calcAge(birthYear) {
     console.log(firstName);
 
     function printAge() {
-        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
         console.log(output);
 
         if (birthYear >= 1981 && birthYear <= 1996) {
             var millenial = true;
+
+
+            // Creating NEW variable with same name as outer scope's variable
             const firstName = 'Mahmud'; // js first look into current scope then perform scope chain. if it is found in the current scope, then lookup does not work 😉
+            
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
 
@@ -192,10 +196,14 @@ function calcAge(birthYear) {
             {
                 return a + b;
             }
+
+            // const output = 'NEW OUTPUT!';
+            output = 'NEW OUTPUT!';
         }
         // console.log(str); str is not defined because const and str are block scoped! 
         console.log(millenial);
         //console.log(add(2, 3)); Reference error
+        console.log(output); // NEW OUTPUT!
     }
 
     printAge();
