d39add7b58937841c86b7025a342b5a385be72d2
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-06-06 14:34:40 +0600

    The this keyword in Practice

--- complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -188,7 +188,7 @@ function calcAge(birthYear) {
 
             // Creating NEW variable with same name as outer scope's variable
             const firstName = 'Mahmud'; // js first look into current scope then perform scope chain. if it is found in the current scope, then lookup does not work 😉
-            
+
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
 
@@ -213,5 +213,206 @@ function calcAge(birthYear) {
 
 const firstName = 'Khalid';
 calcAge(1996);
-// console.log(age); 
+// console.log(age);
 // printAge();
+
+
+// ✈️✈️ Variable Environment: Hoisting and The TDZ
+
+// An Execution context always contains three parts.
+
+// EXECUTION CONTEXT
+// i. 👉 Variable environment 
+//ii.Scope Chain 
+//iii.this keyword
+
+// we already learned Scope Chain. let's learn Variable Environment. In particular how variables are created in javascript.
+// in js, we have a mechanism called 'hoisting'.
+
+
+
+// Hoisting: Makes some types of variables accessible/usable in the code before they are actually declared. "Variables lifted to the top of their scope."
+
+// ⬇️ Behind the Scenes ⬇️
+
+// Before execution, code is scanned for variable declarations and for each variable a new property is created in the "variable environment object".
+
+// behind the scenes the code is basically scanned for variable declarations before it is executed.  This happens during the so called creation phase of the execution context that we talked before.
+
+// Then for each variable that is found in the code, a new property is created in a variable environment object. That's how hoisting works.
+
+
+// many people simply define hoisting by saying that variables are magically lifted or moved to the top of their scope for example, to the top of a function. And that is actually what hoisting looks like on the surface.But behind the scenes that's in fact not what happens. Instead, behind the scenes the code is basically scanned for variable declarations before it is executed.
+
+// But hoisting does not work the same for all the variable types.
+
+// Follow
+
+// we still tried to access the variable while in the TDZ like we actually do in the first line of this if block, then we get a reference error telling us that we can't access job before initialization.
+// So exactly as we learned in the last slide, right ? However, if we tried to access a variable that was actually never even created, like in the last line here where we want to log x, then we get a different error message saying that x is not defined at all.What this means is that job is in fact in the Temporal Dead Zone where it is still initialized,
+// but the engine knows that it will eventually be initialized because it already read the code before and set the job variable in the variable environment to uninitialized. Then when the execution reaches the line where the variable is declared, it is removed from the Temporal Dead Zone.
+
+/*
+const myName = 'Khalid';
+
+if (myName === 'Khalid') {
+    console.log(`Khalid is a ${job}`); // Reference error, can not access 'job' before initialization
+    const age = 2037 - 1989;
+    console.log(age);
+
+    const job = 'teacher';
+    console.log(x); // Reference error: x is not defined
+}
+
+*/
+
+
+// ✈️✈️Hoisting and TDZ in Practice
+
+// Hoisting with variables
+
+console.log(me); // undefined
+
+console.log(job); // Reference error: can not access before initialization. (Job var. is still in the temporal death zone at this point. Temporal Death Zone of a var. is declared with let or const)
+
+console.log(year); // Reference error: can not access before initialization (year var. is still in the temporal death zone at this point. Temporal Death Zone of a var. is declared with let or const)
+
+var me = 'Khalid';
+let job = 'programmer';
+const year = 1991;
+
+// Hoisting with Functions
+
+console.log(addDecl(2, 3)); // 5; we were able to call the 'function declaration' before it was actually defined here in the code.
+
+console.log(addExpr(2, 3)); // Uncaught Reference Error: Can't access 'addExpr' before initialization. (because js treats function as variable and it is simple a const variable too! addExpr() is in temporal death zone!)
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+console.log(addVarFunctionExpression(2, 3)); // Uncaught TypeError: addVarFunctionExpression is not a function
+console.log(addVarFunctionArrow(2, 3)); // Uncaught TypeError: addVarFunctionArrow is not a function
+
+// Reason? because var will be hoisted and set to undefined. here we are trying to call undefined basically.
+// undefined(2,3) 
+// this is why we are getting '.... not a function' error
+
+var addVarFunctionExpression = function (a, b) {
+    return a + b;
+} 
+
+var addVarFunctionArrow = (a, b) => a + b;
+
+// So, hoisting actually works with function declaration!
+
+// Why we should not use var? here is the  example:
+
+// when there is no shopping cart, delete this shopping cart!
+
+console.log(numProducts); // undefined
+
+if (!numProducts) { // undefined due to hoisting. it treats like this if(!undefined) 
+    deleteShoppingCart(); // it will be called even if have 10 products! 
+    // why that happen? it's because of hoisting. 
+}
+
+var numProducts = 10; 
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+// Don't use var to declare a variable.
+// Use const most of the time to declare variables.
+// and let if you really need to change a variable later.
+// ☢️☢️ In order to write clean code, you should declare your variables at the top of each scope. ☢️☢️
+// ☢️ Always declare all your functions first and use them only after the declaration. This applies to all type of functions. even function declarations which are hoisted. ☢️
+
+
+// another example
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(window);
+
+console.log(x === window.x); // true
+console.log(y === window.y); //false 
+console.log(z === window.z); //false
+
+// this proves that variable declared with var keyword creates a property on window object.
+
+
+
+//✈️✈️ The this keyword. (How the THIS keyword works)
+
+// EXECUTION CONTEXT
+// i.  Variable environment
+//ii.Scope Chain
+//iii. 👉 this keyword
+
+// this keyword/variable: Special variable is created for every execution context (every function). Takes the value of (points to) the "owner" of the function in which the this keyword is used.
+
+// this is not static. it depends on how the function is called and its value is only assigned when the function is actually called.
+
+// Method 👉 this = <Object that is calling the method>
+// Simple function call 👉 this = undefined (in strict mode, otherwise window object of the browser.)
+//  Arrow functions 👉 this = <this of surrounding function (lexical this) ; arrow function does not have this
+// Event listener 👉 this = <DOM element that is handler is attached to>
+
+// another way of calling function in js
+// new, call, apply, bind
+
+
+
+// ✈️✈️ The this keyword in Practice
+// this keyword always point to the object that is calling the method.
+console.log(this); 
+
+const calcAge = function (birthYear){
+    console.log(2037 - birthYear);
+    console.log(this); // undefined
+}
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this); // window
+    // because arrow function use this keyword of it's parent scope 
+    // arrow function does not have own this keyword.
+}
+
+calcAgeArrow(1997);
+
+
+const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(this); // jonas object
+        console.log(2037 - this.year);
+    }
+}
+jonas.calcAge(); 
+
+// this keyword always point to the object that is calling the method. Prove?
+
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+// as you can see this keyword is really dynamic. it is not static and depend on how the functions is called.
+
+const f = jonas.calcAge;
+f();
+
