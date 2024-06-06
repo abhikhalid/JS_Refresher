ec22a67479e5559a7a3c8bf42abc9c1501f7003f
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-06-06 19:05:30 +0600

    Primitives vs. Objects in Practice

--- complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -524,3 +524,50 @@ friend.age = 27;
 
 console.log('Friend:', friend);
 console.log('Me', me);
+
+
+
+// ✈️✈️ Primitives vs. Objects in Practice
+
+// Primitive types
+let lastName = 'Ulrich';
+let oldLastName = lastName;
+lastName = 'Davis';
+
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+
+const marriedJessica = jessica; // we are just actually copying the reference
+marriedJessica.lastName = 'Davis';
+
+console.log('Before marriage: ', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// Copying objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Devis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
+
+// Object.assing() only copies at the first level.
+
+
+// So in essence, both the objects, Jessica2 and JessicaCopy have a property called family, which points at the same object in the memory heap, and that object is, of course, this array.
+// And so, when we change the array in one of them, it's also gonna be changed in the other one. Now, a deep clone is what we would need here, but it is not easy to achieve, we do deep clone using an external library like Lo-Dash
