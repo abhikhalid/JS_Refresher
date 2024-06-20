f68204152a98ac16033d14a1e6830cd03030545c
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-06-20 17:42:56 +0600

    Implementation Search Results - Part 1

---
+++ complete-javascript-course-master/complete-javascript-course-master/18-forkify/starter/src/js/model.js
@@ -0,0 +1,61 @@
+import { async } from 'regenerator-runtime/runtime'; //for polyfilling async/await
+import { API_URL } from './config';
+import { getJSON } from './helper';
+
+export const state = {
+    recipe: {},
+    search: {
+        query: '',
+        results: [],
+    },
+};
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await getJSON(`${API_URL}/${id}`);
+
+        // console.log(res, data);
+        let { recipe } = data.data;
+
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cooking_time: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+        }
+
+        console.log(state.recipe);
+    } catch (err) {
+        // console.log(`${err} 💥💥`);
+        throw err;
+    }
+}
+
+export const loadSearchResults = async function (query) {
+    try {
+        state.search.query = query;
+
+        const data = await getJSON(`${API_URL}?search=${query}`);
+        console.log(data);
+
+        state.search.results = data.data.recipes.map(rec => {
+            return {
+                id: rec.id,
+                title: rec.title,
+                publisher: rec.publisher,
+                image: rec.image_url,
+            };
+        });
+
+        console.log(state.search.results);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+//  loadSearchResults('pizza');
